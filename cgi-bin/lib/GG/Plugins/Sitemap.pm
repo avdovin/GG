b3bf7c10d7e0da38f0bc01286f644fc9e9f5cdb7
Aleksey <aleksey@MacBook-Air-Aleksej.local>
2014-02-01 12:31:04 +0400

    new site map plugin

--- cgi-bin/lib/GG/Plugins/Sitemap.pm
+++ cgi-bin/lib/GG/Plugins/Sitemap.pm
@@ -8,71 +8,71 @@ use Mojo::Base 'Mojolicious::Plugin';
 
 sub register {
 	my ($self, $app, $conf) = @_;
-	
-	
+
+
 	$app->routes->route("sitemap.xml")->to( cb => sub{
 		my $self   = shift;
-		
+
 		my $CONFIG = {
-			'texts_main_ru'	=> {
-								'route'		=> 'text',
-								'fields'	=> { 'alias' => 'alias', 'link' => 'link', 'url_for' => 'url_for'},
-								'where'		=> "`viewtext`='1'",
-								'priority'	=> '0.5',
-								'changefreq'	=> 'monthly',
-								}, 
-			'data_catalog_items'	=> {
-								'route'		=> 'catalog_iteminfo',
-								'fields'	=> { 'ID' => 'item_id'},
-								'where'		=> "`active`='1'",
-								'priority'	=> '0.6',
-								'changefreq'	=> 'weekly',
-								},
-			'data_catalog_looks'	=> {
-								'route'		=> 'catalog_lookinfo',
-								'fields'	=> { 'ID' => 'item_id'},
-								'where'		=> "`active`='1'",
-								'priority'	=> '0.6',
-								'changefreq'	=> 'weekly',
-								},			
-			'data_catalog_groups'	=> {
-								'route'		=> 'catalog_lookinfo',
-								'fields'	=> { 'ID' => 'item_id'},
-								'where'		=> "`active`='1'",
-								'priority'	=> '0.6',
-								'changefreq'	=> 'weekly',
-								},	
+			'text'					=> {
+							'table'			=> 'texts_main_ru',
+							'placeholders'	=> { 'alias' => 'alias', 'link' => 'link', 'url_for' => 'url_for'},
+							'where'			=> " `viewtext`='1' ",
+							'priority'		=> '0.5',
+							'changefreq'	=> 'monthly',
+			},
 		};
-		
-		my $host = $self->req->headers->host();
+
+		my $host = $self->host;
 		my $nodes;
-		foreach my $table (keys %$CONFIG){
-			my $priority = $CONFIG->{$table}->{priority} || '0.5';
-			my $changefreq = $CONFIG->{$table}->{changefreq} || 'monthly';
-			my $fields = $CONFIG->{$table}->{fields};
-			my $fields_str = join(",", keys %$fields);
-			
-			for my $node ( $self->app->dbi->query("SELECT $fields_str, `edate`,`rdate` FROM `$table` WHERE ".$CONFIG->{$table}->{where} )->hashes ){
+		foreach my $route (keys %$CONFIG){
+			my $routeConfig = $CONFIG->{ $route };
+
+			my $dopWhere = " `rdate` ";
+			$dopWhere .= ",`edate`" if $self->dbi->exists_keys(from => $$routeConfig{table}, lkey => 'edate');
+
+			my $placeholders = $routeConfig->{placeholders};
+			my $fields_str = join(",", keys %$placeholders);
+			$fields_str .= ','.$fields_str if $fields_str;
+
+			for my $row ( $self->app->dbi->query("SELECT $dopWhere $fields_str FROM `$$routeConfig{table}` WHERE ".$routeConfig->{where} )->hashes ){
 				my $url_vals = {};
-				$node->{edate} = $node->{rdate} if ($node->{edate} eq '0000-00-00 00:00:00');
-				foreach (keys %{$CONFIG->{$table}->{fields}}){
-					$url_vals->{$CONFIG->{$table}->{fields}->{$_}} = $node->{$_};
-				}
-				my $url;
-				if($node->{url_for}){
-					$url = 'http://'.$host.$self->menu_item($node);
-				} else {
-					$url = 'http://'.$host.$self->url_for($CONFIG->{$table}->{route}, %$url_vals);
+				$row->{edate} = $row->{rdate} if (!$row->{edate} or $row->{edate} eq '0000-00-00 00:00:00');
+
+				foreach (keys %$placeholders){
+					$url_vals->{ $placeholders->{$_} } = $row->{$_};
 				}
 
-				$nodes .= $self->render_partial( node => $node, url => $url, priority => $priority, changefreq => $changefreq, template => 'Sitemap/node', format => 'xml');
+				my $url = '';
+				if($row->{link}){
+					$url = $row->{link};
+				}
+				elsif($row->{url_for}){
+					$url = 'http://'.$host.$self->menu_item($row);
+				}
+				else {
+					$url = 'http://'.$host.$self->url_for($route, %$url_vals);
+				}
+
+				my $priority = $routeConfig->{priority} || '0.5';
+				my $changefreq = $routeConfig->{changefreq} || 'monthly';
+
+				$nodes .= $self->render(
+					node 		=> $row,
+					url 		=> $url,
+					priority 	=> $priority,
+					changefreq 	=> $changefreq,
+					template 	=> 'Sitemap/node',
+					format 		=> 'xml',
+
+					partial 	=> 1,
+				);
+
 			}
 		}
-		
-		$self->render( nodes => $nodes, template => 'Sitemap/sitemap', format => 'xml');	
-		
-	})->name('sitemap');
-		
+
+		$self->render( nodes => $nodes, template => 'Sitemap/sitemap', format => 'xml');
+	});
 }
 
 1;
