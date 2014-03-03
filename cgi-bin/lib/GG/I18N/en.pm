0ffb13126d1dad311bf39369129542a1208c55c9
Xorg1984 <vdovin.aa.@gmail.com>
2014-03-03 15:24:20 +0400

    fix bug in i18n lexicon

--- cgi-bin/lib/GG/I18N/en.pm
+++ cgi-bin/lib/GG/I18N/en.pm
@@ -1,6 +1,6 @@
 package GG::I18N::en;
 
-use Mojo::Base 'App::I18N';
+use Mojo::Base 'GG::I18N';
 
 our %Lexicon = ( _AUTO => 1,
 	hello2 => 'Hello two'
--- cgi-bin/lib/GG/I18N/ru.pm
+++ cgi-bin/lib/GG/I18N/ru.pm
@@ -1,9 +1,8 @@
 package GG::I18N::ru;
 
-use Mojo::Base 'App::I18N';
+use Mojo::Base 'GG::I18N';
 use utf8;
 
-die 3;
 our %Lexicon = ( _AUTO => 1,
 	hello => 'Привет',
 	hello2 => 'Привет два'
