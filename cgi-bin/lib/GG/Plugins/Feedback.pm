aaabc7c539195c98089a673f0928d2dc3e51fef3
Xorg1984 <vdovin.a.a@gmail.com>
2014-09-16 20:07:51 +0400

    updated Feedback plugin

--- cgi-bin/lib/GG/Plugins/Feedback.pm
+++ cgi-bin/lib/GG/Plugins/Feedback.pm
@@ -4,25 +4,27 @@ use utf8;
 
 use Mojo::Base 'Mojolicious::Plugin';
 
-
-
 sub register {
 	my ($self, $app, $conf) = @_;
-
-
-	$app->routes->route("feedback")->to(seo_title_sitename => 1, admin_name => 'Контакты', layout => 'default', alias =>  'contacts', cb => sub{
+  
+  $app->routes->get("/feedback")->to(seo_title_sitename => 1, admin_name => 'Контакты', layout => 'default', alias =>  'feedback', cb => sub{
 		my $self   = shift;
 		my %params = @_;
 
-		$self->feedback_form(%params)
+		$self->feedback_form(%params)    
+  })->name('feedback');
+  
+  $app->routes->post("/feedback")->to(alias =>  'feedback', cb => sub{
+		my $self   = shift;
+		my %params = @_;
 
-	})->name('feedback');
-
-
-	$app->helper(
-		feedback_form => sub {
+		$self->feedback_form(%params, submit => 1)    
+  })->name('feedback_submit');
+  
+	$app->helper( feedback_form => sub {
 			my $self = shift;
 			my %params = (
+        submit    => 0,
 				template	=> "Plugins/Feedback/form",
 				@_
 			);
@@ -34,11 +36,14 @@ sub register {
 			$self->meta_keywords( $page->{keywords} );
 			$self->meta_description( $page->{description} );
 
-			my $method = $self->req->method;
-
 			$self->stash->{success} = 0;
 
-			if($method eq 'POST'){
+			if(delete $params{'submit'}){
+        # Check CSRF token
+        my $validation = $self->validation;
+        return $self->render(text => 'Bad CSRF token!', status => 403)
+          if $validation->csrf_protect->has_error('csrf_token');
+          
 				my $send_params = $self->req->params->to_hash;
 				my $json = {
 					errors 	=> {},
@@ -92,8 +97,9 @@ sub register {
 						data    => $email_body,
 					);
 
-					$json->{message_success} = $self->render_to_string(
+					$json->{message_success} = $self->render(
 						template 	=> 'Plugins/Feedback/_message_success',
+						partial 	=> 1,
 					);
 				}
 
@@ -108,8 +114,7 @@ sub register {
 				page 		=> $page,
 			);
 
-		}
-	);
+	});
 }
 
 
