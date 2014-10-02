54d2de88358f46e1bfc82f66260e3b6cf1b0cef5
Xorg1984 <vdovin.a.a@gmail.com>
2014-10-02 16:12:53 +0400

    Updated Geo plugin

--- cgi-bin/lib/GG/Plugins/Geo.pm
+++ cgi-bin/lib/GG/Plugins/Geo.pm
@@ -4,7 +4,10 @@ use utf8;
 
 use Mojo::Base 'Mojolicious::Plugin';
 
+use Socket;
 our $VERSION = '1';
+
+# !!!IMPORT GEO BASE FIRST!!!
 
 sub register {
 	my ( $self, $app, $opts ) = @_;
@@ -12,97 +15,65 @@ sub register {
 	$opts ||= {};
 
 
-	$app->helper( geoLoadData => sub {
+	$app->helper( geo__chech_ip => sub {
 		my $self 	= shift;
-		my $record 	 = shift;
+    return unless my $ip    = shift || $self->ip;
+    
+    my $ip_int = ip2long($ip);
+    $ip_int =~ s{\D+}{}gi;
 
-		my $result = {};
-		my @cities = ();
-		open IN, "<../GEO/cities_utf.txt" or die "Could not open cities.txt!";
-		while (my $kuku = <IN>) {
-			chomp($kuku);
-			my @mas = split /\t/, $kuku;
-			$cities[$mas[0]]->{'city'} = $mas[1];
-			$cities[$mas[0]]->{'region'} = $mas[2];
-			$cities[$mas[0]]->{'district'} = $mas[3];
-			$cities[$mas[0]]->{'coordinates'} = $mas[4].",".$mas[5];
-		}
-		close IN;		
-
-		my @inetnums = ();
-		my $i = 0;
-
-		open IN, "<../GEO/cidr_optim_utf.txt" or die "Could not open cidr_optim.txt!";
-		while (my $kuku = <IN>) {
-			chomp($kuku);
-			my @mas = split /\t/, $kuku;
-			$inetnums[$i]->{'start'} = $mas[0];
-			$inetnums[$i]->{'stop'} = $mas[1];
-			$inetnums[$i]->{'inetnum'} = $mas[2];
-			$inetnums[$i]->{'country'} = $mas[3];
-			$inetnums[$i]->{'cityid'} = $mas[4];
-			$i++;
-		}
-		close IN;
-
-		$record =~ s/\s//gs;
-		my $ADDRESS = ip_to_number($record);
-		if (defined($ADDRESS)) {
-			my $MAXINDEX = $i;
-			my $MININDEX = 0;
-			my $isfinded = 0;
-			my $cc       = 0;  # Iterations counter
-			my $CURRENTINDEX;
-			while ($MININDEX < $MAXINDEX) {
-				$cc++;
-				$CURRENTINDEX = int(($MININDEX + $MAXINDEX)/2);
-				if ($ADDRESS < $inetnums[$CURRENTINDEX]->{'start'}) {
-					$MAXINDEX = $CURRENTINDEX;
-					next;
-				}
-				if ($ADDRESS > $inetnums[$CURRENTINDEX]->{'stop'}) {
-					$MININDEX = $CURRENTINDEX + 1;
-					next;
-				}
-				if (($inetnums[$CURRENTINDEX]->{'start'} <= $ADDRESS) && ($ADDRESS <= $inetnums[$CURRENTINDEX]->{'stop'})) {
-					$isfinded = 1;
-					last;
-				}
-			}
-			if ($isfinded) {
-				$result->{range} = $inetnums[$CURRENTINDEX]->{'inetnum'};
-				$result->{country} = $inetnums[$CURRENTINDEX]->{'country'};
-				if ($inetnums[$CURRENTINDEX]->{'cityid'} ne "-") {
-					my $id = $inetnums[$CURRENTINDEX]->{'cityid'};
-					$result->{city} = $cities[$id]->{'city'};
-					$result->{region} = $cities[$id]->{'region'};
-					$result->{distinct} = $cities[$id]->{'district'};
-					$result->{coord} = $cities[$id]->{'coordinates'};
-
-				} 		
-			}
-
-		}
-		return $result;
+    my $result = {
+      country_id  => 0,
+      city_id     => 0,
+      city_name   => '',
+      country_name  => '',
+    };
+    # Ищем по российским и украинским городам
+    my $sql = "select * from (select * from net_ru where begin_ip<=$ip_int order by begin_ip desc limit 1) as t where end_ip>=$ip_int";
+    if(my $row = $self->dbi->query($sql)->hash){
+      my $result->{'city_id'} = $row->{'city_id'};
+      $sql = "select * from net_city where id='$$result{city_id}'"; 
+      if(my $row = $self->dbi->query($sql)->hash){
+        $result->{'city_name'} = $row->{'name_ru'};
+        $result->{'country_id'} = $row->{'country_id'};
+      } 
+    }
+    # Если не нашли - ищем страну и город по всему миру
+    unless($result->{'city_id'}) {
+      $sql = "select * from (select * from net_euro where begin_ip<=$ip_int order by begin_ip desc limit 1) as t where end_ip>=$ip_int";
+      if(my $row = $self->dbi->query($sql)->hash){
+        $result->{'country_id'} = $row->{'country_id'};
+      }
+      else{
+        # Ищем страну в мире
+        $sql = "select * from (select * from net_country_ip where begin_ip<=$ip_int order by begin_ip desc limit 1) as t where end_ip>=$ip_int";
+        if(my $row = $self->dbi->query($sql)->hash){
+          $result->{'country_id'} = $row->{'country_id'};    
+        }
+      }
+      
+      # Ищем город в глобальной базе
+      $sql = "select * from (select * from net_city_ip where begin_ip<=$ip_int order by begin_ip desc limit 1) as t where end_ip>=$ip_int";
+      if(my $row = $self->dbi->query($sql)->hash){
+        $result->{'city_id'} = $row->{'city_id'};
+        $sql = "select * from net_city where id='$$result{city_id}'";
+        if(my $row = $self->dbi->query($sql)->hash){
+          $result->{'city_name'} = $row->{'name_ru'};
+          $result->{'country_id'} = $row->{'country_id'};
+        } 
+      }
+    }
+    
+    return $result;
 	});
-
-
 }
 
-sub ip_to_number {
-	my $ip = shift;
-	
-	unless($ip) {return undef;}
-	$ip =~ s/\s//gs;
-	$ip =~ s/\,/\./g; 
-	if ($ip =~ /^((([01]?\d{1,2})|(2([0-4]\d|5[0-5])))\.){3}(([01]?\d{1,2})|(2([0-4]\d|5[0-5])))$/) {
-		my @bits = split /\./, $ip;
-		my $ADDRESS = $bits[0]*256*256*256 + $bits[1]*256*256 + $bits[2]*256 + $bits[3];
-		return $ADDRESS;
-	} else {
-		return undef;
-	}
+sub ip2long {
+  return unpack("N",inet_aton(shift))
+}
 
+sub long2ip {
+  return inet_ntoa(pack("N*", shift));
 }
 
 1;
