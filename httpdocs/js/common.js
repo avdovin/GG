4861298b9e5b4e37f101dc1b5752a755485ea15a
Xorg1984 <vdovin.a.a@gmail.com>
2015-10-14 18:44:29 +0300

    code style fix

--- httpdocs/js/common.js
+++ httpdocs/js/common.js
@@ -1,67 +1,70 @@
 // INIT GG
 if (typeof GG != 'undefined') {
-	GG = new GG();
-	GG.init({
-		local: true,
-		debug: true,        // включить режим debug (вывод через log(), а так же другие возможности)
-		setcurrent: true,   // включить автоматическое преобразование .current -> .first__class_current
-		autocurrent: true,  // автоматический включатор текущего элемента типа .some_class_current
+  GG = new GG();
+  GG.init({
+    local: true,
+    debug: true, // включить режим debug (вывод через log(), а так же другие возможности)
+    setcurrent: true, // включить автоматическое преобразование .current -> .first__class_current
+    autocurrent: true, // автоматический включатор текущего элемента типа .some_class_current
 
-		// этот callback вызывается каждый раз после загрузки страницы (в т.ч. при переходе с помощью ajax)
-		afterLoad: function(){
+    // этот callback вызывается каждый раз после загрузки страницы (в т.ч. при переходе с помощью ajax)
+    afterLoad: function() {
 
-			// mainpage gallery
-			if ($.galleryCarousel) $(".top__banners").galleryCarousel(
-				{
-					effect: "slide",
-					dots: true,
-					cb: {
-						before: function(){
-							$(".top__texts .texts__entry:visible").fadeOut(1000);
-						},
-						after: function(current, next){
-							$(".top__texts .texts__entry_"+next)
-								.stop()
-								.fadeIn(500)
-								.find(".entry__title")
-								.css("left", "-100%")
-								.animate({left: 0})
-								.end()
-								.find(".entry__text")
-								.css("right", "-100%")
-								.animate({right: 0});
-						}
-					}}
-			);
-			//if ($.galleryCarousel) $(".top__texts").galleryCarousel({effect: "slide"});
-			//
+      // mainpage gallery
+      if ($.galleryCarousel) $(".top__banners").galleryCarousel({
+        effect: "slide",
+        dots: true,
+        cb: {
+          before: function() {
+            $(".top__texts .texts__entry:visible").fadeOut(1000);
+          },
+          after: function(current, next) {
+            $(".top__texts .texts__entry_" + next)
+              .stop()
+              .fadeIn(500)
+              .find(".entry__title")
+              .css("left", "-100%")
+              .animate({
+                left: 0
+              })
+              .end()
+              .find(".entry__text")
+              .css("right", "-100%")
+              .animate({
+                right: 0
+              });
+          }
+        }
+      });
+      //if ($.galleryCarousel) $(".top__texts").galleryCarousel({effect: "slide"});
+      //
 
-			// gallery prettyPhoto
-			if ($.prettyPhoto) $("a[rel^='prettyPhoto']").prettyPhoto({social_tools: ''});
+      // gallery prettyPhoto
+      if ($.prettyPhoto) $("a[rel^='prettyPhoto']").prettyPhoto({
+        social_tools: ''
+      });
 
-			// phone mask
-			if (typeof jQuery.fn.mask != 'undefined' && $(".field__input_phone").length) $(".field__input_phone").mask('+7 (000) 000-00-00');
-			//
+      // phone mask
+      if (typeof jQuery.fn.mask != 'undefined' && $(".field__input_phone").length) $(".field__input_phone").mask('+7 (000) 000-00-00');
+      //
 
-			// kartochka cloudZoom start
-			if (typeof CloudZoom != 'undefined' && $(".cloudzoom").is(":visible")) CloudZoom.quickStart();
+      // kartochka cloudZoom start
+      if (typeof CloudZoom != 'undefined' && $(".cloudzoom").is(":visible")) CloudZoom.quickStart();
 
-			$(".menu .menu__entry").hover(
-				function(){
-					$(this).addClass("hover");
-				},
-				function(){
-					$(this).removeClass("hover");
-				}
-			);
+      $(".menu .menu__entry").hover(
+        function() {
+          $(this).addClass("hover");
+        },
+        function() {
+          $(this).removeClass("hover");
+        }
+      );
 
-		}
-	});
+    }
+  });
 }
 
-// custom code
-$(function(){
 
-
-
+$(function() {
+	// custom code
 });
--- httpdocs/js/controllers/feedback.js
+++ httpdocs/js/controllers/feedback.js
@@ -1,27 +1,27 @@
-$(function(){
-    GG.attachForm(".contacts__form", ".contacts__form .form__send");
-    ymaps.ready( YMapsInit );
+$(function() {
+  GG.attachForm(".contacts__form", ".contacts__form .form__send");
+  ymaps.ready(YMapsInit);
 });
 
 
 // Yandex Maps
 if (typeof ymaps != 'undefined') {
-    ymaps.ready(YMapsInit);
+  ymaps.ready(YMapsInit);
 }
 
 var myMap,
-    myPlacemark;
+  myPlacemark;
 
-function YMapsInit(){
-    myMap = new ymaps.Map("Gmap", {
-        center: [59.936026,30.318344],
-        zoom: 14
-    });
+function YMapsInit() {
+  myMap = new ymaps.Map("Gmap", {
+    center: [59.936026, 30.318344],
+    zoom: 14
+  });
 
-    myPlacemark = new ymaps.Placemark([59.936026,30.318344], {
-        hintContent: 'Невский проспект, 15'
-    });
+  myPlacemark = new ymaps.Placemark([59.936026, 30.318344], {
+    hintContent: 'Невский проспект, 15'
+  });
 
-    myMap.geoObjects.add(myPlacemark);
+  myMap.geoObjects.add(myPlacemark);
 }
 //
