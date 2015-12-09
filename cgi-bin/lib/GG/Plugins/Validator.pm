370168bb9f4373468d417821bc850049df6cdaf4
Xorg1984 <vdovin.a.a@gmail.com>
2015-12-09 12:57:58 +0300

    move validation to plugin

--- cgi-bin/lib/GG/Plugins/Validator.pm
+++ cgi-bin/lib/GG/Plugins/Validator.pm
@@ -15,6 +15,7 @@ my %RESERVED = map { $_ => 1 } (
 
 use Mojo::Util qw(url_escape trim squish);
 use Encode qw(encode);
+use Regexp::Common qw /URI/;
 
 sub register {
   my ($self, $app, $args) = @_;
@@ -31,6 +32,46 @@ sub register {
       return shift->app->send_params;
     }
   );
+
+  my $validator = $app->validator;
+  $validator = $validator->add_check(is_decimal => sub {
+    my ($validation, $name, $value) = @_;
+    return $value =~ /^(?:\d+(?:\.\d*)?|\.\d+)$/ ? undef : 1;
+  });
+  $validator = $validator->add_check(is_integer => sub {
+    my ($validation, $name, $value) = @_;
+    return $value =~ /^\d+$/ ? undef : 1;
+  });
+  $validator = $validator->add_check(is_human_date => sub {
+    my ($validation, $name, $value) = @_;
+    $value =~ /^\d{2}\.\d{2}\.\d{4}$/ ? undef : 1;
+  });
+  $validator = $validator->add_check(is_base64_image => sub {
+    my ($validation, $name, $value) = @_;
+    return $value =~ m~^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})$~ ? undef : 1;
+  });
+  $validator = $validator->add_check(is_url => sub {
+    my ($validation, $name, $value) = @_;
+    return $value =~ /$RE{URI}{HTTP}/ ? undef : 1;
+  });
+  $validator = $validator->add_check(is_user_email_exist => sub {
+    my ($validation, $name, $value) = @_;
+
+    my $sth = $app->dbh->prepare("select id from data_users where email like ? limit 1");
+    my $valid = 0;
+    $valid = 1 if($sth->execute($value) ne '0E0');
+    $sth->finish();
+    return $valid;
+  });
+  $validator = $validator->add_check(range => sub {
+    my ($validation, $name, $value, $min, $max) = @_;
+    return $value < $min || $value > $max;
+  });
+  $validator = $validator->add_check(gt_to => sub {
+    my ($validation, $name, $value, $to) = @_;
+    return 1 unless defined(my $other = $validation->input->{$to});
+    return int($value) < int($other);
+  });
 
   $app->helper(
     validate => sub {
