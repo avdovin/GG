1a935968b3216be8e0ca3af55558c4f4abf22719
Roman Novoselov <rnovoselov93@gmail.com>
2016-04-13 13:54:13 +0300

    Changed texts controller to use with models

--- cgi-bin/lib/GG/Content/Texts.pm
+++ cgi-bin/lib/GG/Content/Texts.pm
@@ -6,16 +6,7 @@ use Mojo::Base 'GG::Content::Controller';
 sub redirect_to_first_item {
   my $self = shift;
 
-  $self->stash->{key_razdel} ||= 'news';
-
-  my $db_table = 'texts_' . $self->stash->{key_razdel} . '_' . $self->lang;
-
-  return $self->render_not_found unless my $item = $self->dbi->query( qq/
-		SELECT `alias`
-		FROM `$db_table`
-		WHERE `viewtext`='1' ORDER BY `tdate` DESC
-		LIMIT 0,1
-	/)->hash;
+  return $self->render_not_found unless my $item = $self->m( $self->stash('model') || 'Main' )->get_last();
 
   $self->redirect_to($self->stash->{key_razdel} . '/' . $item->{alias});
 }
@@ -62,119 +53,47 @@ sub texts_list {
     : 1,
     limit => 0
     ,   # $self->get_var(name => 'news_limit', controller => 'texts', raw => 1),
+    model       => ucfirst $self->stash('key_razdel'),
     @_
   );
 
   my $key_razdel = delete $params{key_razdel};
-  my $table
-    = "texts_"
-    . $key_razdel . "_"
-    . ($params{lang_tables} ? $self->lang : 'ru');
   my $template = $key_razdel . '_list';
 
+  my $main_page = {};
+
   if ($self->stash->{alias}) {
-
-    if (
-      my $mainPage = $self->dbi->query(
-        "SELECT `name`,`title`,`keywords`,`description` FROM `texts_main_ru` WHERE `alias`='"
-          . $self->stash->{alias}
-          . "' AND `viewtext`='1'"
-      )->hash
-      )
-    {
-
-      $self->meta_title($mainPage->{title} || $mainPage->{name});
-      $self->meta_keywords($mainPage->{keywords});
-      $self->meta_description($mainPage->{description});
+    if ( $main_page = $self->m('Main')->get_by_alias( $self->stash->{alias} ) ){
+      $self->meta_title($main_page->{title} || $main_page->{name});
+      $self->meta_keywords($main_page->{keywords});
+      $self->meta_description($main_page->{description});
     }
-
   }
+  # Условия выборки:
+  my $where = { viewtext => 1 };
 
+  $where->{"YEAR($params{order_field})"} = $self->param('year') if $self->param('year');
 
-  if ($self->stash->{level}) {
-    my $level = $self->stash->{level};
-    $template .= "_level_$level";
+  my %p = (
+    where     => $where,
+    order     => ( $params{order_field} ? "`$params{order_field}` $params{order_desc} " : ""),
+  );
 
-    my $last_alias = $self->stash->{'alias_' . $level};
-    my $last_item
-      = $self->dbi->query(
-      "SELECT `dir` FROM `$table` WHERE `viewtext`='1' AND `alias`='$level'")
-      ->hash;
-    return $self->render_not_found unless $last_item;
-
-    unless ($last_item->{dir}) {
-      return $self->item_by_alias(%params);
-    }
-
-    $self->stash->{$key_razdel . '_' . $level} = $last_item;
-
-    $params{where}
-      = " AND `texts_"
-      . $key_razdel . "_"
-      . $self->lang
-      . "`='$$last_item{ID}' ";
-
-#		foreach my $i (1..$deep){
-#			my $alias = $self->stash->{'alias_'.$i};
-#		}
-  }
-
-
-# Условия выборки:
-  my $where = " `viewtext`='1' ";
-  $where .= " AND `dir`='0'"
-    if $self->dbi->exists_keys(from => $table, lkey => 'dir');
-  $where .= " AND YEAR($params{order_field})='" . $self->param('year') . "' "
-    if $self->param('year');
-  $where .= " AND MONTH($params{order_field})='" . $self->param('month') . "' "
-    if $self->param('month');
-  $where .= " $params{where} "
-    if $params{where}
-    ;    # Дополнительные параметры выборки
-
-  # Список с учетом выбранной папки (по alias)
-  if ($self->stash->{group_alias}) {
-    return $self->render_not_found
-      unless my $parentDir = my $mainPage
-      = $self->dbi->query("SELECT * FROM `$table` WHERE `dir`='1' AND `alias`='"
-        . $self->stash->{group_alias}
-        . "' AND `viewtext`='1'")->hash;
-
-    $where .= " AND `$table`='$$parentDir{ID}' ";
-    $self->metaHeader(
-      title => $parentDir->{title} || $parentDir->{name},
-      keywords    => $parentDir->{keywords},
-      description => $parentDir->{description}
-    );
-  }
-
-  $where
-    .= " ORDER BY "
-    . ($self->dbi->exists_keys(from => $table, lkey => 'rating')
-    ? ' `rating`,'
-    : '')
-    . "`$params{order_field}` $params{order_desc} "
-    . ($params{order_field} ne 'ID' ? ",`ID` desc" : "");
   if ($params{limit}) {
-    my $count = $self->dbi->getCountCol(from => $table, where => $where);
+    my $count = $self->m($params{model})->count( $where);
     $self->def_text_interval(
       total_vals   => $count,
       cur_page     => $params{page},
       col_per_page => $params{limit}
     );
     $params{npage} = $params{limit} * ($params{page} - 1);
-    $where .= " LIMIT $params{npage},$params{limit} ";
+
+    $p{offset} = $params{npage};
   }
 
-  my $items = $self->dbi->query(
-    qq/
-		SELECT *
-		FROM `$table`
-		WHERE $where
-	/
-  )->hashes;
+  my $items = $self->m("$params{model}")->get_list(%p);
 
-  $self->render(template => "texts/$template", items => $items);
+  $self->render(template => "texts/$template", items => $items, main_page => $main_page);
 
 }
 
@@ -183,15 +102,8 @@ sub text_main_item {
   my $alias = $self->stash('alias');
 
 
-  my $where = "`viewtext`='1'";
-  $where .= " AND `alias`='$alias'";
-
   return $self->render_not_found
-    unless my $text
-    = $self->dbi->query(
-    "SELECT *, DATE_FORMAT(updated_at,'%a, %d %b %Y %T') AS updated_at_rfc822 FROM `texts_main_"
-      . $self->lang
-      . "` WHERE $where")->hash;
+    unless my $text = $self->m('Main')->get_by_alias( $self->stash('alias') );
 
   #AJAX request
   return $self->render(text => $text->{text}) if ($self->req->is_xhr);
@@ -205,10 +117,6 @@ sub text_main_item {
   $self->meta_keywords($text->{keywords});
   $self->meta_description($text->{description});
 
-# Wed, 24 Sep 2014 19:15:45 GMT
-#$self->res->headers->last_modified( $text->{updated_at_rfc822}.' GMT' )
-#  if ($text->{'updated_at'} && $text->{'updated_at'} ne '0000-00-00 00:00:00'  && !(scalar keys %{$self->userdata}));
-
   my $template = $self->stash->{template} ||= 'texts/_body_default';
   $self->render(
     item     => $text,
@@ -220,47 +128,15 @@ sub text_main_item {
 
 sub text_list_item {
   my $self = shift;
-  my %params = (where => "`ID`='" . $self->stash('ID') . "'", @_);
+  my %params = ( @_ );
 
-  my $ring ||= 0;
   my $alias      = $self->stash('list_item_alias');
-  my $key_razdel = $self->stash('key_razdel');
-  my $template   = "texts/" . $key_razdel . "_item";
-  my $table      = "texts_" . $key_razdel . "_" . $self->lang;
+  my $template   = "texts/" . $self->stash('key_razdel') . "_item";
 
-  my $where = " ";
-  $where .= " AND `dir`='0'"
-    if $self->dbi->exists_keys(from => $table, lkey => 'dir');
-  $where .= " AND `viewtext`='1' ";
-
-
-#	my $order = "ID";
-#	foreach (qw (tdate)){
-#		if ($self->dbi->exists_keys(from => $table, lkey => $_)) {
-#			$order = $_;
-#			last;
-#		}
-#	}
-
-
-  return $self->render_not_found unless my $item = $self->dbi->query(
-    qq/
-			SELECT *, DATE_FORMAT(updated_at,'%a, %d %b %Y %T') AS updated_at_rfc822
-			FROM `$table`
-			WHERE `alias`='$alias' $where LIMIT 1/
-  )->hash;
+  return $self->render_not_found unless my $item = $self->m( ucfirst $self->stash('key_razdel') )->get_by_alias( $self->stash('list_item_alias') );
 
   if ($self->stash->{alias}) {
-    if (
-      my $textSection = $self->dbi->query(
-            "SELECT `name`,`title` FROM `texts_main_"
-          . $self->lang
-          . "` WHERE `alias`='"
-          . $self->stash->{alias}
-          . "' AND `viewtext`='1'"
-      )->hash
-      )
-    {
+    if ( my $textSection = $self->m('Main')->get_by_alias( $self->stash('alias') ) ){
       $self->meta_title($textSection->{title} || $textSection->{name});
     }
   }
@@ -268,12 +144,6 @@ sub text_list_item {
   $self->meta_title($item->{title} || $item->{name});
   $self->meta_keywords($item->{keywords});
   $self->meta_description($item->{description});
-
-#$self->res->headers->last_modified( $item->{updated_at_rfc822}.' GMT' )
-#  if ($item->{'updated_at'} && $item->{'updated_at'} ne '0000-00-00 00:00:00'  && !(scalar keys %{$self->userdata}));
-
-#$text->{index_after} = $self->get_index_after(from => $table, index => $ID, ring => $ring, where => $where, order => $order);
-#$text->{index_befor} = $self->get_index_befor(from => $table, index => $ID, ring => $ring, where => $where, order => $order);
 
   $self->render(item => $item, template => $template);
 }
